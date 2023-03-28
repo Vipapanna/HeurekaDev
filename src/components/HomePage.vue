73170d35bb8a1caa66786803e4e0d69ae2b77b79
maxiiiktsu <maxim.fruhwald@gmail.com>
2023-03-28 09:05:00 +0200

    omtg

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,5 +1,24 @@
 <template>
+<<<<<<< Updated upstream
     <h1 class="text-3xl font-bold underline">
         PICUS
     </h1>
 </template>
+=======
+    
+    <Searchbar />
+
+
+<h1>ahojteseca</h1>
+
+</template>
+
+<script>
+import Searchbar from './Searchbar.vue'
+
+export default {
+  components: { Searchbar },
+    
+}
+</script>
+>>>>>>> Stashed changes
