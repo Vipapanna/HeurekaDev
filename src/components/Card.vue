a98da49a12bbc92d5113d49fdd309ccfbd482ec8
maxiiiktsu <maxim.fruhwald@gmail.com>
2023-04-04 10:32:18 +0200

    ok

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,5 +1,9 @@
 <template>
+<<<<<<< Updated upstream
     <div class=" flex flex-col w-[19rem] m-4 drop-shadow-md ">
+=======
+    <div class=" flex flex-col w-[19rem] m-4 drop-shadow-md" >
+>>>>>>> Stashed changes
       <img class="w-[19rem] h-[11rem] rounded-t-2xl" src="src/assets/images/mcdonald-card.png" alt="">
       
       <div class="bg-[#F8EDE3] rounded-b-2xl">
@@ -15,4 +19,6 @@
       </div>
 
     </div>
+
+
 </template>
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,4 +1,5 @@
 <template>
+<<<<<<< Updated upstream
   
   <section class="bg-[#4C4556] flex    justify-end   h-20" >
     
@@ -16,20 +17,52 @@
 
   <section class=" bg-black bg-opacity-50 absolute inset-0  hidden justify-center items-center  " id="overlay"> 
     
+=======
+  <section >
+    <!--navbar -->
+    
+    <section class="bg-[#4C4556] flex h-20 items-center justify-between" >    
+      
+      <object data="./src/assets/images/vipapanna1.svg" type="image/svg+xml"  class="  m-4 mr-[rem] " > logo</object>
+      
+      <Searchbar/>
+      
+      <button id="login" class="  w-24  h-12 tex  rounded-2xl bg-white bg-opacity-20  drop-shadow-md text-white font-sans self-center mr-4 hover:opacity-90 ">
+        LogIn
+      </button>
+      
+    </section>
+    
+    <!-- log in popup -->
+    <section class=" bg-black bg-opacity-50 absolute inset-0 hidden justify-center items-center  " id="overlay"> 
+      <div></div>
+>>>>>>> Stashed changes
       
       <div class="bg-white rounded-2xl  flex flex-col w-[24rem] h-[28rem] justify-center items-center  " id="login-page" >
-
+        
         <Backbtn id="back-btn" class="mt-4"  />
+<<<<<<< Updated upstream
 
         <input type="email" class="h-8 rounded-3xl  self-center  bg-gray-400 bg-opacity-20  drop-shadow-md  w-[18rem] m-2  text-center" placeholder="example@gmail.com"  name="" id="e1">
         <input type="password" class="h-8 rounded-3xl self-center  bg-gray-400 bg-opacity-20  drop-shadow-md  w-[18rem] m-2 text-center " placeholder="Heslo"  name="" id="p1">
 
         <div class="   space-x-24 mb-9 " >
+=======
+        
+        <!-- login a signup text pole -->
+        
+        <input type="email" class="h-8 rounded-3xl  self-center  bg-gray-400 bg-opacity-20  drop-shadow-md  w-[18rem] m-2  text-center" placeholder="example@gmail.com"  name="" id="e1">
+        <input type="password" class="h-8 rounded-3xl self-center  bg-gray-400 bg-opacity-20  drop-shadow-md  w-[18rem] m-2 text-center " placeholder="Heslo"  name="" id="p1">
+        
+        
+        <div class="space-x-24 mb-9 " >
+>>>>>>> Stashed changes
           
           <button class="  bg-transparent text-[0.8rem]  text-blue-500" id="create-account" >Vytvorit ucet</button>
           <button class="  bg-transparent text-[0.8rem]  text-blue-500" id="forgotten-password" >Zabudnute heslo</button>
-
+          
         </div>
+<<<<<<< Updated upstream
 
 
 
@@ -40,32 +73,98 @@
                   
       <div class="bg-white rounded-2xl  flex-col w-[24rem] h-[28rem] justify-center items-center  hidden "  id="register-page" >
 
+=======
+        
+        
+        <button class=" rounded-xl bg-gray-400 bg-opacity-20 w-28 h-9 hover:opacity-80 ">LogIn</button>
+        
+        
+      </div>
+      
+      
+      <div class="bg-white rounded-2xl flex-col w-[24rem] h-[28rem] justify-center items-center hidden "  id="register-page" >
+        
+>>>>>>> Stashed changes
         <Backbtn id="back-btn" />
         
         <input type="email" class="h-8 rounded-3xl  self-center  bg-gray-400 bg-opacity-20  drop-shadow-md  w-[18rem] m-2  text-center" placeholder="example@gmail.com"  name="" id="e1">
         <input type="password" class="h-8 rounded-3xl self-center  bg-gray-400 bg-opacity-20  drop-shadow-md  w-[18rem] m-2 text-center " placeholder="Heslo"  name="" id="p1">
         <input type="password" class="h-8 rounded-3xl self-center  bg-gray-400 bg-opacity-20  drop-shadow-md  w-[18rem] m-2 text-center " placeholder="znova zadanie Heslo"  name="" id="p1">
-
+        
         <button class=" rounded-xl bg-gray-400 bg-opacity-20 w-32 h-10 hover:opacity-80 mt-10 " >Registracia</button>
-
-      </div>
-
-      <div class="bg-white rounded-2xl  flex-col w-[24rem] h-[28rem] justify-center items-center  hidden "  id="reset-password-page" >
-
-        <Backbtn id="back-btn" />
-
-        <h1 class="  mb-14 text-center " >Na zadanú emailovu adresu vám bude <br> zaslaný link na obnovu vašeho hesla</h1>
-        <input type="email" class="h-8 rounded-3xl  self-center  bg-gray-400 bg-opacity-20  drop-shadow-md  w-[18rem] m-2  text-center" placeholder="example@gmail.com"  name="" id="e1">
-
-
-        <button class=" rounded-xl bg-gray-400 bg-opacity-20 w-32 h-10 hover:opacity-80 mt-10 " >Registracia</button>
-
+        
       </div>
       
+      <div class="bg-white rounded-2xl  flex-col w-[24rem] h-[28rem] justify-center items-center  hidden "  id="reset-password-page" >
+        
+        <Backbtn id="back-btn" />
+        
+        <h1 class="  mb-14 text-center " >Na zadanú emailovu adresu vám bude <br> zaslaný link na obnovu vašeho hesla</h1>
+        <input type="email" class="h-8 rounded-3xl  self-center  bg-gray-400 bg-opacity-20  drop-shadow-md  w-[18rem] m-2  text-center" placeholder="example@gmail.com"  name="" id="e1">
+        
+        
+        <button class=" rounded-xl bg-gray-400 bg-opacity-20 w-32 h-10 hover:opacity-80 mt-10 " >Registracia</button>
+        
+      </div>
+      
+<<<<<<< Updated upstream
     
         
+=======
+    </section>
+    
+    
+    <section class="flex overflow-auto scrollbar-hide" >
+      
+      
+      <img src="/src/assets/images/banner1.jpeg"  class= "h-96 rounded-lg m-4 " alt="">
+      <img src="/src/assets/images/banner2.jpeg" class= " h-96 rounded-lg m-4" alt="">
+      <img src="/src/assets/images/banner3.jpeg" class= " h-96 rounded-lg m-4" alt="">
+      <img src="/src/assets/images/banner4.jpeg" class= " h-96 rounded-lg m-4" alt="">
+      
+      
+    </section>
+    
+    <section class=" grid grid-cols-4 gap-0  mx-16">
+
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </section>
+    
+    <section class="bg-[#4C4556] flex flex-row h-60 " >
+
+      <div class=" grid grid-cols-2 gap-0   h-36 self-center " >
+        <button class=" text-white m-0 text-2xl" >Cookies</button>
+        <button class=" text-white m-0 text-2xl" >About us</button>
+        <button class=" text-white m-0 text-2xl" >FAQs</button>
+        <button class=" text-white m-0 text-2xl" >Contact us</button>
+      </div>
+      
 
 
+
+>>>>>>> Stashed changes
+
+
+<<<<<<< Updated upstream
 
   </section>
 
@@ -127,9 +226,11 @@
 
     
 
+=======
+>>>>>>> Stashed changes
     
-
     
+<<<<<<< Updated upstream
 
   </section>
   
@@ -141,9 +242,23 @@
     
 
 
+=======
+    
+    
+    
+    
+    
+    
+    
+    
+</section>
+>>>>>>> Stashed changes
+
 
 
 </template>
+
+
 
 <script>
 
@@ -209,3 +324,5 @@ export default {
     
 }
 </script>
+
+
